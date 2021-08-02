d787b44c6ded02fb947bb32ff2805e4930791392
mbt-tech <mbaradji221@outlook.fr>
2021-08-02 14:56:53 +0200

    add _addNode in findInContextByType

--- dist/GraphManagerService.js
+++ dist/GraphManagerService.js
@@ -239,7 +239,11 @@ class GraphManagerService {
    */
     findInContextByType(startId, contextId, nodeType) {
         return this.findInContext(startId, contextId, (node) => {
-            return node.getType().get() === nodeType;
+            if (node.getType().get() === nodeType) {
+                this._addNode(node);
+                return true;
+            }
+            return false;
         });
     }
     /**
